88c6e584361a9cce1ce79c0c6727e5ea945f2533
xd720p <petr.kozlov@wukkerteam.com>
2017-01-24 11:05:54 +0300

    -VK Group API init

--- vkCallbackServer.js
+++ vkCallbackServer.js
@@ -1,8 +1,14 @@
 let express = require('express');
 let http = require('http');
-let https = require('https');
 let config = require('./configs/vkCallbackServerConfig.json');
 let bodyParser = require('body-parser');
+const fs = require('fs');
+const https = require('https');
+
+const options  = {
+    key: fs.readFileSync('/etc/letsencrypt/live/136335.simplecloud.club/privkey.pem'),
+    cert: fs.readFileSync('/etc/letsencrypt/live/136335.simplecloud.club/cert.pem')
+};
 
 let callbackServer = express();
 callbackServer.use(bodyParser.json());
@@ -10,7 +16,8 @@ callbackServer.use(bodyParser.urlencoded({extended: false}));
 callbackServer.set('port', config.server.port);
 
 
-https.createServer(callbackServer).listen(callbackServer.get('port'), function (err) {
+
+https.createServer(callbackServer).listen(options, callbackServer.get('port'), function (err) {
     if (err) throw err;
     console.log('Server listening on port ' + callbackServer.get('port'));
 });
