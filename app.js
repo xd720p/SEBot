b5e722ec9e4f73f606390dd527c8bd6ae8f08516
xd720p <petr.kozlov@wukkerteam.com>
2017-01-25 11:20:10 +0300

    -VK Group API init

--- app.js
+++ app.js
@@ -11,62 +11,8 @@ mongoose.connect(config.mongoDB.prefix + config.mongoDB.host + config.mongoDB.po
 
 let botDB = mongoose.connection;
 
+let vkCallbackApi = require('./vk_echo/vkCallbackServer').vkCallbackServer;
 
-let bodyParser = require('body-parser');
-let fs = require('fs');
-let https = require('https');
-let http = require('http');
-let vkConfig = require('./configs/vkCallbackServerConfig.json');
-
-let callbackServer = express();
-callbackServer.use(bodyParser.json());
-callbackServer.use(bodyParser.urlencoded({extended: false}));
-callbackServer.set('httpport', vkConfig.server.http);
-callbackServer.set('httpsport', vkConfig.server.https);
-
-const options  = {
-    key: fs.readFileSync('/etc/letsencrypt/live/136335.simplecloud.club/privkey.pem', 'utf8'),
-    cert: fs.readFileSync('/etc/letsencrypt/live/136335.simplecloud.club/cert.pem', 'utf8'),
-    ca: fs.readFileSync('/etc/letsencrypt/live/136335.simplecloud.club/chain.pem', 'utf8')
-};
-
-https.createServer(options, callbackServer).listen(callbackServer.get('httpsport'), function (err) {
-    if (err) throw err;
-    console.log('Https listening on port ' + callbackServer.get('httpsport'));
-});
-
-callbackServer.post('/', function (req, res, next) {
-    console.log('Request: ', req.body);
-    if (isVkApi(req)) {
-        res.send("208b5a5c");
-    } else if (isVkNewPost(req)) {
-        res.status(200).send("ok");
-        console.log('new_vk_post');
-        sendPostToTelegram(req);
-    } else {
-        console.log('other event');
-        res.status(200).send("ok");
-    }
-});
-
-callbackServer.get('/', function (req, res, next) {
-    console.log('Request: ', req.body);
-    res.send("Hello world");
-});
-
-function sendPostToTelegram(req) {
-    let post = req.body.object.text;
-
-    app.telegram.sendMessage('37729716', post);
-}
-
-function isVkApi(req) {
-    return (req.body.type == vkConfig.vkposts.access.type && req.body.group_id == vkConfig.vkposts.access.group_id);
-}
-
-function isVkNewPost(req) {
-    return req.body.type == vkConfig.vkposts.wall_post_new.type;
-}
 
 botDB.on('error', console.error.bind(console, 'connection error:'));
 botDB.once('open', function() {
--- vk_echo/vkCallbackServer.js
+++ vk_echo/vkCallbackServer.js
@@ -5,79 +5,73 @@
 // let https = require('https');
 // let http = require('http');
 
-let vkCallbackServer = {
+let vkCallbackServer = function () {
 
+    let vkConfig = null;
+    let https = null;
+    let http = null;
 
-    init: function () {
-        vkCallbackServer.express = require('express');
-        vkCallbackServer.vkConfig = require('./../configs/vkCallbackServerConfig.json');
-        vkCallbackServer.bodyParser = require('body-parser');
-        vkCallbackServer.fs = require('fs');
-        vkCallbackServer.https = require('https');
-        vkCallbackServer.http = require('http');
-        vkCallbackServer.callbackServer = vkCallbackServer.express();
-        vkCallbackServer.callbackServer.use(vkCallbackServer.bodyParser.json());
-        vkCallbackServer.callbackServer.use(vkCallbackServer.bodyParser.urlencoded({extended: false}));
-        vkCallbackServer.callbackServer.set('httpport', vkCallbackServer.vkConfig.server.http);
-        vkCallbackServer.callbackServer.set('httpsport', vkCallbackServer.vkConfig.server.https);
-    },
+    return {
+        init: function () {
+            let express = require('express');
+            let bodyParser = require('body-parser');
+            vkConfig = require('./../configs/vkCallbackServerConfig.json');
+            https = require('https');
+            http = require('http');
+            this.callbackServer = express();
+            this.callbackServer.use(bodyParser.json());
+            this.callbackServer.use(bodyParser.urlencoded({extended: false}));
+            this.callbackServer.set('httpport', vkConfig.server.http);
+            this.callbackServer.set('httpsport', vkConfig.server.https);
+        },
 
-    createServer: function () {
-        vkCallbackServer.options = {
-            key: vkCallbackServer.fs.readFileSync('/etc/letsencrypt/live/136335.simplecloud.club/privkey.pem', 'utf8'),
-            cert: vkCallbackServer.fs.readFileSync('/etc/letsencrypt/live/136335.simplecloud.club/cert.pem', 'utf8'),
-            ca: vkCallbackServer.fs.readFileSync('/etc/letsencrypt/live/136335.simplecloud.club/chain.pem', 'utf8')
-        };
+        makeServer: function () {
+            let fs = require('fs');
+            let options = {
+                key: fs.readFileSync('/etc/letsencrypt/live/136335.simplecloud.club/privkey.pem', 'utf8'),
+                cert: fs.readFileSync('/etc/letsencrypt/live/136335.simplecloud.club/cert.pem', 'utf8'),
+                ca: fs.readFileSync('/etc/letsencrypt/live/136335.simplecloud.club/chain.pem', 'utf8')
+            };
 
-        vkCallbackServer.https.createServer(vkCallbackServer.options, vkCallbackServer.callbackServer).listen(vkCallbackServer.callbackServer.get('httpsport'), function (err) {
-            if (err) throw err;
-            console.log('Https listening on port ' + vkCallbackServer.callbackServer.get('httpsport'));
-        });
+            https.createServer(options, this.callbackServer).listen(this.callbackServer.get('httpsport'), function (err) {
+                if (err) throw err;
+                console.log('Https listening on port ' + this.callbackServer.get('httpsport'));
+            });
+        }
     }
 
 };
 
-vkCallbackServer.init();
-vkCallbackServer.createServer();
+module.exports.vkCallbackServer = vkCallbackServer;
+
+let p = vkCallbackServer();
+
+p.callbackServer.post('/', function (req, res, next) {
+    console.log('Request: ', req.body);
+   if (isVkApi(req)) {
+        res.send("208b5a5c");
+   } else if (isVkNewPost(req)) {
+       res.status(200).send("ok");
+       console.log('new_vk_post');
+   } else {
+       console.log('other event');
+       res.status(200).send("ok");
+   }
+});
+
+p.callbackServer.get('/', function (req, res, next) {
+    console.log('Request: ', req.body);
+    res.send("Hello world");
+});
 
 
-// const options  = {
-//     key: fs.readFileSync('/etc/letsencrypt/live/136335.simplecloud.club/privkey.pem', 'utf8'),
-//     cert: fs.readFileSync('/etc/letsencrypt/live/136335.simplecloud.club/cert.pem', 'utf8'),
-//     ca: fs.readFileSync('/etc/letsencrypt/live/136335.simplecloud.club/chain.pem', 'utf8')
-// };
-//
-// https.createServer(options, callbackServer).listen(callbackServer.get('httpsport'), function (err) {
-//     if (err) throw err;
-//     console.log('Https listening on port ' + callbackServer.get('httpsport'));
-// });
-//
-// callbackServer.post('/', function (req, res, next) {
-//     console.log('Request: ', req.body);
-//    if (isVkApi(req)) {
-//         res.send("208b5a5c");
-//    } else if (isVkNewPost(req)) {
-//        res.status(200).send("ok");
-//        console.log('new_vk_post');
-//    } else {
-//        console.log('other event');
-//        res.status(200).send("ok");
-//    }
-// });
-//
-// callbackServer.get('/', function (req, res, next) {
-//     console.log('Request: ', req.body);
-//     res.send("Hello world");
-// });
-//
-//
-// function isVkApi(req) {
-//     return (req.body.type == vkConfig.vkposts.access.type && req.body.group_id == vkConfig.vkposts.access.group_id);
-// }
-//
-// function isVkNewPost(req) {
-//     return req.body.type == vkConfig.vkposts.wall_post_new.type;
-// }
-//
-//
+function isVkApi(req) {
+    return (req.body.type == vkConfig.vkposts.access.type && req.body.group_id == vkConfig.vkposts.access.group_id);
+}
+
+function isVkNewPost(req) {
+    return req.body.type == vkConfig.vkposts.wall_post_new.type;
+}
+
+
 // module.exports.callbackServer = callbackServer;
