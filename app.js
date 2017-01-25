78c370b8bc48edcd88264e53b92626631a64dd05
xd720p <petr.kozlov@wukkerteam.com>
2017-01-25 13:56:53 +0300

    -VK Group API init

--- app.js
+++ app.js
@@ -39,45 +39,6 @@ telegramBot.bot.command('quit', (ctx) => {
 
 });
 
-// function saveUserToBd(ctx) {
-//     let user = new User({
-//         _id: ctx.from.id,
-//         first_name: ctx.from.first_name,
-//         last_name: ctx.from.last_name,
-//         username: ctx.from.username,
-//         subscription: true
-//     });
-//
-//     User.findOne({_id: ctx.from.id}, function (err, result) {
-//        if (!result) {
-//           user.save (function(err, savedUser, affected) {
-//                if (err) {
-//                    throw err;
-//                } else {
-//                    console.log(savedUser);
-//                    ctx.reply(greetings(ctx, 'Welcome, '))
-//                }
-//            });
-//        } else {
-//            ctx.reply(greetings(ctx, 'Welcome back, '));
-//        }
-//     });
-//
-// }
-
-// function greetings(ctx, message) {
-//     if (ctx.from.first_name != undefined) {
-//         if (ctx.from.last_name != undefined) {
-//             return message +  ctx.from.first_name + ' ' + ctx.from.last_name;
-//         } else if (ctx.from.username != undefined) {
-//             return message +  ctx.from.username;
-//         } else {
-//             return message +  ctx.from.first_name;
-//         }
-//     } else {
-//         return message +  ctx.from.username;
-//     }
-// }
 
 telegramBot.bot.command('help', (ctx) => {
     console.log('help', ctx.from);
@@ -89,22 +50,10 @@ telegramBot.bot.hears('spam', (ctx) => {
     console.log('Spam was initiated by ', ctx.from);
     telegramBot.sendSpamToAll(ctx.from.username);
 });
-//
-// function sendSpamToAll(username) {
-//     User.find({subscription: true}, function (err, foundUsers) {
-//         foundUsers.forEach(function (item, i, arr) {
-//             app.telegram.sendMessage(item._id, "Spam from @" + username);
-//         })
-//     });
-// }
-//
-
 telegramBot.bot.catch((err) => {
-    //ctx.reply('Shit happens. Soon all will be fixed');
+
     console.log('Ooops', err)
 });
 
 telegramBot.bot.startPolling();
 
-
-// module.exports = app;
