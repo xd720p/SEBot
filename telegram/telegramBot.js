fa195fc4e0df937240d99536737aafcf8e596990
xd720p <petr.kozlov@wukkerteam.com>
2017-01-26 12:27:17 +0300

    -VK Group API init

--- telegram/telegramBot.js
+++ telegram/telegramBot.js
@@ -40,7 +40,7 @@ let telegramBot = function () {
             telegramBot.replyToSender(ctx, 'This is help!');
         });
 
-        that.bot.telegram.onText(/\/echo (.+)/, function (msg, match) {
+        that.bot.hears(/\/echo (.+)/, function (msg, match) {
             telegramBot.replyToSender(msg.chat.id, match[1]);
         });
 
